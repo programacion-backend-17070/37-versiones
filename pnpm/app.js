2ab9f6caa0eb0a8db621fa5cefb2e445e83ac22a
Eduardo Velazquez <lalo0892@gmail.com>
2022-05-15 20:25:44 -0500

    delete all

--- pnpm/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/", (req, res) => res.send('fui creado por pnpm'))
-
-app.listen(8080, () => console.log('listening'))
--- yarn/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/", (req, res) => res.send('fui creado por yarn'))
-
-app.listen(8080, () => console.log('listening'))
